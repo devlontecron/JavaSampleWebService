10b963c113e9677a7c1f8006d90bfee86c38985d
Devin <Devin@LenovoFlex>
2017-10-09 17:31:17 -0700

    calcsRunner runs each calc level

--- test/calcsRunner.sh
+++ test/calcsRunner.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
-calcs=$1
-loops=$2
 
+   calcs[1]='100'
+   calcs[2]='1000'
+   calcs[3]='10000'
+   calcs[4]='100000'
+   calcs[5]='250000'
+   calcs[6]='20'
+   calcs[7]='100'
+   calcs[8]='1000'
 
-for((i = 1; i<=10; i++))
+   loops[1]='20'
+   loops[2]='20'
+   loops[3]='20'
+   loops[4]='20'
+   loops[5]='20'
+   loops[6]='500000'
+   loops[7]='100000'
+   loops[8]='1000'
+
+for((q = 1; q<=8; q++))
 do
-   calcs=$1
-   loops=$2
-   sleep=0
+
+for((i = 1; i<=13; i++))
+do
+   sleep=10
    thread=($i)
 
-   echo --Running Test $i
-   sudo ./calcs.sh "$calcs" "$loops" "$thread" "$sleep" > results/Ubuntu_C4.Large_C${calcs}_L${loops}_T${thread}.csv
+   echo --Running Test $q.$i
+   sudo ./calcs.sh "${calcs[q]}" "${loops[q]}" "$thread" "$sleep" > results/Ubuntu_C4.Large_C${calcs[q]}_L${loops[q]}_T${thread}.csv
    echo --Test Complete
    
+done
 done
 
