8737d1fcd0f30de0aa8a607bc3afb381bb4013e1
Devin <Devin@LenovoFlex>
2017-10-12 17:41:04 -0700

    fixed bug where avg from dataParse where total/entries+1

--- test/dataParse.sh
+++ test/dataParse.sh
@@ -4,6 +4,6 @@ file=$1
 
 #output=cat $file | cut -d, -f 9,10
 
-total9=`cat $file | awk -F"," '{sum+=$9} {sum2+=$10}  END{print "=",sum,",",sum2,",Total=",sum/NR,",",sum2/NR,",","Avg"}'`
+total9=`cat $file | awk -F"," '{sum+=$9} {sum2+=$10}  END{print "=",sum,",",sum2,",Total=",sum/(NR-1),",",sum2/(NR-1),",","Avg"}'`
 
 echo $total9 | sed 's/=/\n,,,,,,,,/g' 
